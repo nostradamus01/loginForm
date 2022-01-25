94f1930d786fc62511f24a5ba327d1461c8a7c15
nostradamus01 <stepanyan0299@gmail.com>
2022-01-25 23:46:47 +0400

    testing08

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,9 @@ const mainContainer = document.querySelector('.main');
 const signUpHeaderBtn = document.querySelector('.signup-header');
 const loginHeaderBtn = document.querySelector('.login-header');
 
+const loginMsgBox = document.querySelector('#loginMsg');
+const signupMsgBox = document.querySelector('#signupMsg');
+
 signUpHeaderBtn.addEventListener('click', () => {
 	mainContainer.classList.remove('login-active');
 });
@@ -10,11 +13,19 @@ loginHeaderBtn.addEventListener('click', () => {
 	mainContainer.classList.add('login-active');
 });
 
+const getRegistrationData = (data) => {
+	let ob = {};
+	for (let [key, value] of data.entries()) {
+		ob[key] = value;
+	}
+	return ob;
+}
+
 const getFormData = (form) => {
 	let ob = {};
 	let data = new FormData(form);
-	for(let pair of data.entries()) {
-		ob[pair[0]] = pair[1];
+	for (let [key, value] of data.entries()) {
+		ob[key] = value;
 	}
 	return ob;
 }
@@ -30,28 +41,33 @@ const sendRequest = async (url, data) => {
 }
 
 const register = async (userData) => {
-	let response = await sendRequest('/.netlify/functions/mongo/register', userData);
-	console.log(response);
+	return await sendRequest('/.netlify/functions/mongo/register', userData);
 }
 
 const login = async (userData) => {
-	let response = await sendRequest('/.netlify/functions/mongo/login', userData);
-	console.log(response);
+	return await sendRequest('/.netlify/functions/mongo/login', userData);
 }
 
 const loginForm = document.querySelector('#loginForm');
 const registrationForm = document.querySelector('#registrationForm');
 
-loginForm.addEventListener('submit', (e) => {
+loginForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	login(getFormData(e.target)).then(() => {
-		console.log('login done');
-	});
+	let res = await login(getFormData(e.target));
+	console.log(res);
 });
 
-registrationForm.addEventListener('submit', (e) => {
+registrationForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	register(getFormData(e.target)).then(() => {
-		console.log('register done');
-	});
+	let formData = getFormData(e.target);
+	if (formData.password === formData.repassword) {
+		delete formData.repassword;
+		signupMsgBox.innerHTML = ``;
+	} else {
+		signupMsgBox.innerHTML = `Password mismatch`;
+	}
+	console.log(formData);
+	// register().then(() => {
+	// 	console.log('register done');
+	// });
 });
