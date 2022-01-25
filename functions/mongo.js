829648761984a33f3838c80b2a6d5f2c29b3a4a4
nostradamus01 <stepanyan0299@gmail.com>
2022-01-25 23:57:17 +0400

    testing09

--- functions/mongo.js
+++ functions/mongo.js
@@ -25,56 +25,54 @@ const connect = async (callback) => {
 router.post('/register', async (req, res) => {
     let result = null;
     let error = null;
+    let message = null;
     await connect(async () => {
         try {
             result = await createUser(req.body);
         } catch (e) {
+            error = e;
             if (e.code === 11000) {
-                error = 'Email is already used'
+                message = 'Email is already used'
             } else {
-                error = 'There is a problem'
+                message = 'There is a problem. Try again later'
             }
         }
     });
     res.send({
         ok: !error,
-        message: error || 'Good',
-        res: !error ? result.insertedId : ''
+        message: message || error.code
     });
 });
 
 router.post('/login', async (req, res) => {
     let error = null;
     let user = null;
-    let right = false;
+    let login = false;
     let message = '';
     await connect(async () => {
         try {
             user = await getUser(req.body.email);
             if (user) {
-                message = 'Wrong Password';
                 if (user.password === req.body.password) {
-                    right = true;
+                    login = true;
+                    message = 'Correct'
+                } else {
+                    message = 'Password is wrong';
                 }
+            } else {
+                message = "That account doesn't exist";
             }
         } catch (e) {
             error = e;
         }
         res.send({
             ok: !error,
-            message: message || error,
-            res: right,
+            message: message || error.code,
+            login: login
         });
     });
 
 });
-
-const getDatabasesList = async () => {
-    const dbList = await client.db().admin().listDatabases();
-    dbList.databases.forEach((db) => {
-        console.log(`DataBase: ${db.name}`);
-    });
-}
 
 const createUser = async (user) => {
     return await client.db('basa1').collection('collect1').insertOne(user);
